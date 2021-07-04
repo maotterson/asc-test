616f086a53a58faff9fe6c18bf291f2233b9d618
maotterson <otterson.2@gmail.com>
2021-07-03 22:39:17 -0400

    add to message setup

--- api/services/sendteamsmessage.js
+++ api/services/sendteamsmessage.js
@@ -2,14 +2,16 @@ const dotenv = require('dotenv').config();
 const axios = require('axios');
 
 exports.sendCheckInMessage = async (body) => {
-  const checkinid = body.checkinid;
+  const checkinid = body._id;
+  const courseid = body.courseid;
+  const tutor = body.tutorid
   const webhookURI = process.env.TEAMS_WEBHOOK_URI;
   const message = {
     "@context": "https://schema.org/extensions",
     "@type": "MessageCard",
     "themeColor": "0072C6",
     "title": "Tutoring Center: Student Incoming",
-    "text": "Incoming student for [[CISS121]], [[@Aidan Bundy]]",
+    "text": `Incoming student for ${courseid}, ${tutorid}`,
     "potentialAction": [
         {
             "@type": "OpenUri",
